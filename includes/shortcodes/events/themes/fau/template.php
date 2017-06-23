b331ced276a26a7ad80d1278d2d1410c6f0f9d34
rvdforst <rolf.v.d.forst@fau.de>
2017-06-23 11:38:49 +0200

    Update events shortcode.

--- includes/shortcodes/events/themes/fau/template.php
+++ includes/shortcodes/events/themes/fau/template.php
@@ -4,72 +4,74 @@
 defined('ABSPATH') || exit;
 
 $multiday = [];
-
-wp_enqueue_style('rrze-calendar-shortcode-events');
 ?>
-
 <div class="events-list">
     <?php if (empty($events_data)): ?>
     <p><?php _e('Keine bevorstehenden Termine.', 'rrze-calendar'); ?></p>
     <?php else: ?>
-    <div>
-        <?php foreach ($events_data as $date): ?>
-            <?php foreach ($date as $event): ?>
-                <?php if (in_array($event->endpoint_url, $multiday)): ?>
-                    <?php continue; ?>
-                <?php endif; ?>        
+    <ul>
+    <?php foreach ($events_data as $date) : ?>
+        <?php foreach ($date as $event): 
+            if (in_array($event->endpoint_url, $multiday)):
+                continue;
+            endif;            
+	    $bgcolorclass = '';
+	    $inline = '';
+	    if (isset($event->category)) :
+		if (!empty($event->category->bgcol)) :
+		    $bgcolorclass = $event->category->bgcol;
+		elseif (!empty($event->category->color)) :
+		    $inline = 'style="background-color:' . $event->category->color.'"';
+		endif;
+	    endif; ?>
+            <li>                                           
                 <div class="event-item">
-                    <div class="event-title">
-                        <a href="<?php echo $event->endpoint_url; ?>"><?php echo esc_html($event->summary); ?></a>
+                    <div class="event-date <?php echo $bgcolorclass; ?>" <?php echo $inline; ?>>
+                        <span class="event-date-month"><?php echo $event->start_month_html ?></span>
+                        <span class="event-date-day"><?php echo $event->start_day_html ?></span>
                     </div>
-                    <div class="event-title-date">
-                        <?php echo $event->long_start_date ?>
-                    </div>                    
                     <div class="event-info">
                         <?php if ($event->allday) : ?>
-                            <div class="event-date event-allday">
+                            <div class="event-time event-allday">
                                 <?php _e('Ganztägig', 'rrze-calendar'); ?>
                             </div>
                         <?php endif; ?>
                         <?php if ($event->allday && $event->multiday) : ?>
                             <?php $multiday[] = $event->endpoint_url; ?>
-                            <div class="event-date">
+                            <div class="event-time">
                                 <?php echo esc_html(sprintf(__('%1$s bis %2$s', 'rrze-calendar'), $event->long_e_start_date, $event->long_e_end_date)) ?>
                             </div>            
                         <?php elseif (!$event->allday && $event->multiday) : ?>
                             <?php $multiday[] = $event->endpoint_url; ?>
-                            <div class="event-date">
+                            <div class="event-time">
                                 <?php echo esc_html(sprintf( __('%1$s %2$s Uhr bis %3$s %4$s Uhr', 'rrze-calendar'), $event->long_e_start_date, $event->short_e_start_time, $event->long_e_end_date, $event->short_e_end_time)) ?>
                             </div>
                         <?php elseif (!$event->allday): ?>
-                            <div class="event-date">
+                            <div class="event-time">
                                 <?php echo esc_html(sprintf( __('%1$s Uhr bis %2$s Uhr', 'rrze-calendar'), $event->short_start_time, $event->short_end_time)) ?>
                             </div>            
                         <?php endif; ?>
-                        <?php if ($location && $event->location) : ?>
-                            <div class="event-location">
-                                <?php echo esc_html($event->location); ?>
-                            </div>
-                        <?php endif; ?>
-                        <?php if ($description && $event->description) : ?>
-                            <div class="event-summary">
-                                <?php echo make_clickable(wp_trim_words(nl2br($event->description))); ?>
-                            </div>
-                        <?php endif; ?>
+                        <div class="event-title">
+                            <a href="<?php echo $event->endpoint_url; ?>"><?php echo esc_html($event->summary); ?></a>
+                        </div>
+                        <div class="event-location">
+                            <?php echo $event->location ? nl2br($event->location) : '&nbsp;'; ?>
+                        </div>
                     </div>
                 </div>
-            <?php endforeach; ?>
+            </li>
         <?php endforeach; ?>
-        <?php if ($calendar_page_url): ?>
-        <div class="events-more-links">
-            <a class="events-more" href="<?php echo $calendar_page_url; ?>"><?php _e('Mehr Veranstaltungen', 'rrze-calendar'); ?></a>
-        </div>
-        <?php endif; ?>
-    </div>
+    <?php endforeach; ?>
+    </ul>
+    <?php if ($calendar_page_url): ?>
+    <p class="events-more-links">
+        <a class="events-more" href="<?php echo $calendar_page_url; ?>"><?php _e('Mehr Veranstaltungen', 'rrze-calendar'); ?></a>
+    </p>
     <?php endif; ?>
-    <?php if($subscribe_url): ?>
+    <?php endif; ?>
+    <?php if ($calendar_subscribe_url): ?>
     <p class="events-more-links">
         <a class="events-more" href="<?php echo $calendar_subscribe_url; ?>"><?php _e('Abonnement', 'rrze-calendar'); ?></a>
     </p>
-    <?php endif; ?>
+    <?php endif; ?>    
 </div>
