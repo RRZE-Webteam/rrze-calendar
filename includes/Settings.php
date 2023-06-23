32efed2ad93aba99dad11434be59c3097cd3ac5f
rvdforst <rolf.v.d.forst@fau.de>
2023-06-23 08:28:53 +0200

    Update Settings.php

--- includes/Settings.php
+++ includes/Settings.php
@@ -10,19 +10,21 @@ class Settings
 {
     const OPTION_NAME = 'rrze_calendar';
 
-    public static function init()
-    {
-        $settings = new RRZEWPSettings(__('Calendar Settings', 'rrze-settings'), 'rrze-calendar');
+    protected $settings;
 
-        $settings->setCapability('manage_options')
+    public function __construct()
+    {
+        $this->settings = new RRZEWPSettings(__('Calendar Settings', 'rrze-settings'), 'rrze-calendar');
+
+        $this->settings->setCapability('manage_options')
             ->setOptionName(self::OPTION_NAME)
             ->setMenuTitle(__('Calendar', 'rrze-settings'))
             ->setMenuPosition(6)
             ->setMenuParentSlug('options-general.php');
 
-        $settings->addSection(__('ICS Feed', 'rrze-calendar'));
+        $this->settings->addSection(__('ICS Feed', 'rrze-calendar'));
 
-        $settings->addOption('select', [
+        $this->settings->addOption('select', [
             'name' => 'schedule_recurrence',
             'label' => __('Schedule', 'rrze-calendar'),
             'description' => __('Choose the recurrence to check for new events.', 'rrze-calendar'),
@@ -33,6 +35,59 @@ class Settings
             ]
         ]);
 
-        $settings->build();
+        $this->settings->build();
+    }
+
+    public function defaultOptions()
+    {
+        $options = [];
+        foreach ($this->settings->tabs as $tab) {
+            foreach ($tab->sections as $section) {
+                foreach ($section->options as $option) {
+                    $options[$option->args['name']] = $option->args['default'] ?? null;
+                }
+            }
+        }
+
+        return $options;
+    }
+
+    public function getOptions()
+    {
+        $defaults = $this->defaultOptions();
+        $options = get_option(self::OPTION_NAME, []);
+        $options = wp_parse_args($options, $defaults);
+        $options = array_intersect_key($options, $defaults);
+
+        return $options;
+    }
+
+    public function getOption($option)
+    {
+        $options = $this->getOptions();
+        return $options[$option] ?? null;
+    }
+
+    /**
+     * __call method
+     * Method overloading.
+     */
+    public function __call(string $name, array $arguments)
+    {
+        if (!method_exists($this, $name)) {
+            $message = sprintf('Call to undefined method %1$s::%2$s', __CLASS__, $name);
+            do_action(
+                'rrze.log.error',
+                $message,
+                [
+                    'class' => __CLASS__,
+                    'method' => $name,
+                    'arguments' => $arguments
+                ]
+            );
+            if (defined('WP_DEBUG') && WP_DEBUG) {
+                throw new \Exception($message);
+            }
+        }
     }
 }
